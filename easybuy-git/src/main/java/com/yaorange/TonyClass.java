c9b9a773c925700d995f9755eb5c3e475583da8a
dsmguapi <1244075508@qq.com>
2017-05-17 13:57:30 +0800

    tony自己修改自己的穿衣风格

--- easybuy-git/src/main/java/com/yaorange/TonyClass.java
+++ easybuy-git/src/main/java/com/yaorange/TonyClass.java
@@ -4,5 +4,6 @@ public class TonyClass {
 public static void main(String[] args) {
 	System.out.println("tony开发的项目");
 	System.out.println("tony的穿衣风格");
+	System.out.println("tony自己修改自己的穿衣风格");
 }
 }
